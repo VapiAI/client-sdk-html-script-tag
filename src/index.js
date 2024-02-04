9d85446ef760532ec07f5e2f6faa8fd70bc1c911
kaustubh <kaustubhkagrawal@gmail.com>
2024-02-04 10:11:35 +0530

    update config

--- src/index.js
+++ src/index.js
@@ -3,45 +3,43 @@ import defaultAssistant from "./assistant";
 import { createButtonElement } from "./button";
 import { defaultListeners } from "./listeners";
 
-const defaultButtonConfig = {
-  position: "bottom",
-  offset: "40px",
-  width: "50px",
-  height: "50px",
-  idle: {
-    color: `rgb(93, 254, 202)`,
-    type: "pill",
-    title: "Have a quick question?",
-    subtitle: "Click here to talk with our AI assistant",
-    icon: ``,
-  },
-  loading: {
-    color: `rgb(255, 255, 255)`,
-    type: "pill",
-    title: "Connecting...",
-    subtitle: "Please wait",
-    icon: ``,
-  },
-  active: {
-    color: `rgb(255, 255, 255)`,
-    type: "pill",
-    title: "Call is in progress...",
-    subtitle: "Click again to end the call.",
-    icon: ``,
-  },
-};
-
 const runSDK = ({
   apiKey = "",
   assistant = defaultAssistant(),
 
-  button: defaultButtonConfig,
+  buttonConfig = {
+    position: "bottom",
+    offset: "40px",
+    width: "50px",
+    height: "50px",
+    idle: {
+      color: `rgb(93, 254, 202)`,
+      type: "pill",
+      title: "Have a quick question?",
+      subtitle: "Click here to talk with our AI assistant",
+      icon: ``,
+    },
+    loading: {
+      color: `rgb(255, 255, 255)`,
+      type: "pill",
+      title: "Connecting...",
+      subtitle: "Please wait",
+      icon: ``,
+    },
+    active: {
+      color: `rgb(255, 255, 255)`,
+      type: "pill",
+      title: "Call is in progress...",
+      subtitle: "Click again to end the call.",
+      icon: ``,
+    },
+  },
   // position = "bottom",
   // color = `rgb(93, 254, 202)`,
   // offset = "40px",
 }) => {
   const vapi = new Vapi(apiKey);
-  const button = createButtonElement(defaultButtonConfig);
+  const button = createButtonElement(buttonConfig);
 
   defaultListeners(vapi, button, color, assistant);
 
